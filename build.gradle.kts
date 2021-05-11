8f4e2a64de0e6b5b5178139ec22c038cface9c78
Sylvain Bertrand <sbertrand@ihmc.us>
2021-05-11 17:09:41 -0500

    :arrow-up: ihmc-graphics-description 0.19.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ ihmc {
 
 mainDependencies {
    api("us.ihmc:euclid:0.16.2")
-   api("us.ihmc:ihmc-graphics-description:0.19.3")
+   api("us.ihmc:ihmc-graphics-description:0.19.4")
 }
 
 testDependencies {
